cc525e7ece95dc4cc0e7230e05399a099e581810
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2024-03-06 02:14:55 +0800

    sidenav behaviours

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    console.log("document is ready!")
+    $("#burger").on("click", function(e){
+        $("#sidenav").toggleClass("mobile");
+        $("#burger").toggleClass("active");
+        console.log("navbar expand");
+        e.stopPropagation();
+    })
+})
+$('body,html').click(function(e){
+    // console.log("body clicked")
+    $('#sidenav').removeClass('mobile');
+    // doesn't work on mobile chrome but works on desktop version
+ });
+ document.onclick = function(a){
+    let menu_icon_box = document.querySelector("#burger");
+    let box = document.querySelector("#sidenav");
+    if (!menu_icon_box.contains(a.target) && !box.contains(a.target) ) {
+        $('#sidebar').removeClass('mobile');
+        // works on mobile and windows chrome
+    }
+}
