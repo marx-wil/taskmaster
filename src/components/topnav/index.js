9c72e56e6e2a788cf1e919bc2112ec103225047c
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2024-04-07 05:06:49 +0800

    topnav layout design
    glassmorphism design on topnav

--- src/components/topnav/index.js
+++ src/components/topnav/index.js
@@ -1,12 +1,23 @@
-import { Box } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 
-const Topnav = props => {
+const style = {
+  glass: {
+    backgroundImage:
+      'linear-gradient(145deg, rgba(255,255,255,0.5), rgba(255,255,255,0) 70%)', // Adjusted gradient with transparency
+    backdropFilter: 'blur(10px)', // Increased blur effect
+    boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )', // Refined shadow
+    position: 'sticky',
+    top: 10, // Stick to the top
+    zIndex: 10, // Ensure it's above other content
+  },
+};
+
+const Topnav = (props) => {
   return (
-    <>
-      <Box bg="tomato" w="100%" p={4} color="white">
-        This is the Box
-      </Box>
-    </>
+    <Flex style={style.glass} w="100%" p={8} borderRadius="lg" mb="4">
+      {/* Add your content here */}
+    </Flex>
   );
 };
+
 export default Topnav;
--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -3,7 +3,15 @@ import { Box } from '@chakra-ui/react';
 const Dashboard = props => {
   return (
     <>
-      <Box>This is the dashboard</Box>
+      <Box h="200vh">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Box>
     </>
   );
 };
