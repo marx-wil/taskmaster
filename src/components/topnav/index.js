c2d2db216be78978fbf125412ea692879ed9256e
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2024-04-15 22:41:18 +0800

    Update index.js

--- src/components/topnav/index.js
+++ src/components/topnav/index.js
@@ -1,8 +1,4 @@
-import {
-  Flex,
-  Spacer,
-  VStack,
-} from '@chakra-ui/react';
+import { Flex, Spacer, VStack } from '@chakra-ui/react';
 
 import DBreadcrumb from './components/breadcrumb';
 import NavHead from './components/NavHead';
@@ -11,11 +7,10 @@ import NavIcons from './components/navicons';
 const Topnav = props => {
   const style = {
     glass: {
-      backdropFilter: 'blur(10px)', // Increased blur effect
-      // boxShadow: '0 8px 32px 0 rgba( 31, 38, 135, 0.37 )', // Refined shadow
+      backdropFilter: 'blur(10px)',
+      zIndex: 10,
       position: 'sticky',
-      top: '1.4rem', // Stick to the top
-      zIndex: 10, // Ensure it's above other content
+      top: '.25em',
     },
   };
   return (
@@ -25,7 +20,7 @@ const Topnav = props => {
         style={style.glass}
         w="100%"
         py={4}
-        mb="8"
+        mb="4"
         borderRadius="2xl"
         align="center"
       >
