6d600bac2d43063d2518bac3afd21ed4ce4a3b06
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-24 09:22:15 +0800

    chore: removed container

--- src/views/landingpage/index.js
+++ src/views/landingpage/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { Box, Container, useColorModeValue } from '@chakra-ui/react';
 import Hero from '../../components/landingpage/hero';
 import LandingFeatures from '../../components/landingpage/features';
+import HowItWorks from '../../components/landingpage/howitworks';
 
 const LandingPage = () => {
   const bgColor = useColorModeValue('#F5F7FA', '#161B22');
 
   return (
     <Box bg={bgColor}>
-      <Container maxW="container.xl" py={20}>
-        <Hero />
-        <LandingFeatures />
-      </Container>
+      <Hero />
+      <LandingFeatures />
+      <HowItWorks />
     </Box>
   );
 };
 
-export default LandingPage; 
+export default LandingPage;
