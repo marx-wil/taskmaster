e5777bbd20fe94a230ed45c87b90212bc9d4b906
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-24 08:31:58 +0800

    feat: landing page features section

--- src/views/landingpage/index.js
+++ src/views/landingpage/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, Container, useColorModeValue } from '@chakra-ui/react';
 import Hero from '../../components/landingpage/hero';
+import LandingFeatures from '../../components/landingpage/features';
 
 const LandingPage = () => {
   const bgColor = useColorModeValue('#F5F7FA', '#161B22');
@@ -9,6 +10,7 @@ const LandingPage = () => {
     <Box bg={bgColor}>
       <Container maxW="container.xl" py={20}>
         <Hero />
+        <LandingFeatures />
       </Container>
     </Box>
   );
