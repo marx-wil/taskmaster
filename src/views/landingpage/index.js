c1ff531d62f01fd83acd1da341f62693fbbe92a3
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-04-24 11:43:21 +0800

    feat: animate components on scroll

--- src/views/landingpage/index.js
+++ src/views/landingpage/index.js
@@ -5,17 +5,28 @@ import LandingFeatures from '../../components/landingpage/features';
 import HowItWorks from '../../components/landingpage/howitworks';
 import Testimonials from '../../components/landingpage/testimonials';
 import PricingSection from '../../components/landingpage/pricing';
+import ScrollAnimation from '../../components/landingpage/scrollanim/ScrollAnimation';
 
 const LandingPage = () => {
   const bgColor = useColorModeValue('#F5F7FA', '#161B22');
 
   return (
     <Box bg={bgColor}>
-      <Hero />
-      <LandingFeatures />
-      <HowItWorks />
-      <Testimonials />
-      <PricingSection />
+      <ScrollAnimation>
+        <Hero />
+      </ScrollAnimation>
+      <ScrollAnimation>
+        <LandingFeatures />
+      </ScrollAnimation>
+      <ScrollAnimation>
+        <HowItWorks />
+      </ScrollAnimation>
+      <ScrollAnimation>
+        <Testimonials />
+      </ScrollAnimation>
+      <ScrollAnimation>
+        <PricingSection />
+      </ScrollAnimation>
     </Box>
   );
 };
