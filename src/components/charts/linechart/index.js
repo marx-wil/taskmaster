10a873d01f65a090a36613caba8190d24d76a51d
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-05-02 09:18:36 +0800

    refactor: fixed formatting + better readability

--- src/components/charts/linechart/index.js
+++ src/components/charts/linechart/index.js
@@ -36,91 +36,91 @@ const Charts = () => {
   return (
     <>
       <Box>
-      <Card bg={cardBg}>
-        <CardHeader>
-          <Heading
-            size="md"
-            fontFamily="Manrope"
-            fontWeight="600"
-            color={headerColor}
-          >
-            Line Chart
-          </Heading>
-        </CardHeader>
+        <Card bg={cardBg}>
+          <CardHeader>
+            <Heading
+              size="md"
+              fontFamily="Manrope"
+              fontWeight="600"
+              color={headerColor}
+            >
+              Line Chart
+            </Heading>
+          </CardHeader>
 
-        <CardBody>
-          <ResponsiveContainer width="100%" height={300}>
-            <LineChart data={data}>
-              <XAxis
-                dataKey="name"
-                padding={{ left: 10, right: 10 }} // Reduced padding for smaller screens
-                stroke={textColor}
-                tickLine={false}
-                interval={0} // Display all labels
-              />
-              <YAxis
-                stroke={textColor}
-                tickLine={false}
-                domain={[0, 5000]}
-                // Conditionally apply rotation to Y-axis labels
-                angle={rotateYAxisLabels ? -90 : 0}
-                textAnchor="end"
-                hide={useBreakpointValue({ base: true, md: false })} // Hide Y-axis on mobile devices
-              />
-              <Tooltip
-                content={({ label, payload }) => {
-                  // Customizing tooltip content and labels
-                  return (
-                    <div
-                      style={{
-                        background: toolTipBg,
-                        padding: '10px',
-                        borderRadius: '8px',
-                        boxShadow:
-                          '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
-                        border: '1px solid',
-                        borderColor: toolTipBorder,
-                        textAlign: centerTooltip ? 'center' : 'left', // Center tooltip content when in mobile mode
-                      }}
-                    >
-                      <p
+          <CardBody>
+            <ResponsiveContainer width="100%" height={300}>
+              <LineChart data={data}>
+                <XAxis
+                  dataKey="name"
+                  padding={{ left: 10, right: 10 }} // Reduced padding for smaller screens
+                  stroke={textColor}
+                  tickLine={false}
+                  interval={0} // Display all labels
+                />
+                <YAxis
+                  stroke={textColor}
+                  tickLine={false}
+                  domain={[0, 5000]}
+                  // Conditionally apply rotation to Y-axis labels
+                  angle={rotateYAxisLabels ? -90 : 0}
+                  textAnchor="end"
+                  hide={useBreakpointValue({ base: true, md: false })} // Hide Y-axis on mobile devices
+                />
+                <Tooltip
+                  content={({ label, payload }) => {
+                    // Customizing tooltip content and labels
+                    return (
+                      <div
                         style={{
-                          color: textColor,
-                          margin: '0',
-                          fontWeight: '800',
+                          background: toolTipBg,
+                          padding: '10px',
+                          borderRadius: '8px',
+                          boxShadow:
+                            '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+                          border: '1px solid',
+                          borderColor: toolTipBorder,
+                          textAlign: centerTooltip ? 'center' : 'left', // Center tooltip content when in mobile mode
                         }}
                       >
-                        {label}
-                      </p>
-                      {payload.map((entry, index) => (
                         <p
-                          key={index}
-                          style={{ color: textColor, margin: '0' }}
+                          style={{
+                            color: textColor,
+                            margin: '0',
+                            fontWeight: '800',
+                          }}
                         >
-                          {`${entry.name}: ${entry.value}`}
+                          {label}
                         </p>
-                      ))}
-                    </div>
-                  );
-                }}
-              />
-              <Line
-                type="monotone"
-                dataKey="TotalTasks"
-                stroke="#5941CC"
-                strokeWidth={3}
-                activeDot={{ r: 8 }}
-              />
-              <Line
-                type="monotone"
-                dataKey="FinishedTasks"
-                stroke="#4CAF50"
-                strokeWidth={3}
-              />
-            </LineChart>
-          </ResponsiveContainer>
-        </CardBody>
-      </Card>
+                        {payload.map((entry, index) => (
+                          <p
+                            key={index}
+                            style={{ color: textColor, margin: '0' }}
+                          >
+                            {`${entry.name}: ${entry.value}`}
+                          </p>
+                        ))}
+                      </div>
+                    );
+                  }}
+                />
+                <Line
+                  type="monotone"
+                  dataKey="TotalTasks"
+                  stroke="#5941CC"
+                  strokeWidth={3}
+                  activeDot={{ r: 8 }}
+                />
+                <Line
+                  type="monotone"
+                  dataKey="FinishedTasks"
+                  stroke="#4CAF50"
+                  strokeWidth={3}
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          </CardBody>
+        </Card>
       </Box>
     </>
   );
