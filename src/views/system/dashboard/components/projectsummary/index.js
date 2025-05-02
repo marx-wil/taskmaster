436036d9bb45c0d4feb420e99c90592938bc337e
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-05-02 09:09:51 +0800

    refactor: fixed formatting + better readability

--- src/views/system/dashboard/components/projectsummary/index.js
+++ src/views/system/dashboard/components/projectsummary/index.js
@@ -2,18 +2,19 @@ import StatsCard from '../../../../../components/statscard';
 import { FiFolder } from 'react-icons/fi';
 
 const ProjectSummary = () => {
-    const userData = {
-        activeProjects: 5,
-    };
-    return (
-        <>
-        <StatsCard
-            header="Projects Summary"
-            main={userData.activeProjects}
-            icon={FiFolder}
-            footer="Active Projects"
-        />
-        </>)
-}
+  const userData = {
+    activeProjects: 5,
+  };
+  return (
+    <>
+      <StatsCard
+        header="Projects Summary"
+        main={userData.activeProjects}
+        icon={FiFolder}
+        footer="Active Projects"
+      />
+    </>
+  );
+};
 
 export default ProjectSummary;
