54c7ca46040911fecf101f873218d0c06899c598
Wilmarx John Cayabyab <wilmarx.cayabyab@gmail.com>
2025-05-02 09:17:04 +0800

    refactor: fixed formatting + better readability

--- src/components/sidenav/components/ProfileCard/index.js
+++ src/components/sidenav/components/ProfileCard/index.js
@@ -1,48 +1,42 @@
-import {
-  Box,
-  Center,
-  Flex,
-  Stack,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { Box, Center, Flex, Stack, useColorModeValue } from '@chakra-ui/react';
 
 const defProps = {
-  username: "Zhack Dtech",
-  email: "zhack.dtech@gmail.com",
-  image: "https://avatars.githubusercontent.com/u/50767502?v=4",
+  username: 'Zhack Dtech',
+  email: 'zhack.dtech@gmail.com',
+  image: 'https://avatars.githubusercontent.com/u/50767502?v=4',
 };
 const style = {
   ProfileCard: {
-    width: "260px",
-    boxSizing: "border-box",
-    boxShadow: "0px 2px 8px rgba(0,0,0,0.16)",
-    cursor: "pointer",
+    width: '260px',
+    boxSizing: 'border-box',
+    boxShadow: '0px 2px 8px rgba(0,0,0,0.16)',
+    cursor: 'pointer',
   },
   UserText: {
-    fontSize: "14px",
-    fontFamily: "Manrope",
+    fontSize: '14px',
+    fontFamily: 'Manrope',
     fontWeight: 700,
-    padding: "3px 0 0 0",
+    padding: '3px 0 0 0',
   },
   EmailText: {
-    fontSize: "12px",
-    fontFamily: "Manrope",
-    fontWeight: "500",
-    lineHeight: "10px",
-    padding: "3px 0 0 0",
+    fontSize: '12px',
+    fontFamily: 'Manrope',
+    fontWeight: '500',
+    lineHeight: '10px',
+    padding: '3px 0 0 0',
   },
   ImageContainer: {
-    top: "32px",
-    left: "24px",
-    width: "45px",
-    height: "45px",
-    borderRadius: "100000px",
-    boxSizing: "border-box",
-    backgroundPosition: "center center",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
+    top: '32px',
+    left: '24px',
+    width: '45px',
+    height: '45px',
+    borderRadius: '100000px',
+    boxSizing: 'border-box',
+    backgroundPosition: 'center center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
     backgroundImage: `url(${defProps.image})`,
-    cursor: "pointer",
+    cursor: 'pointer',
   },
 };
 function UserDetails({ title, desc, ...rest }) {
@@ -66,13 +60,19 @@ function Username() {
 function Email() {
   return <div style={style.EmailText}>{defProps.email}</div>;
 }
-const ProfileCard = (props) => {
-  let profileCardBg = useColorModeValue("#ffffff","#0B1437")
+const ProfileCard = props => {
+  let profileCardBg = useColorModeValue('#ffffff', '#0B1437');
   return (
     <>
-      <Flex flexDirection="row" style={style.ProfileCard} borderRadius="xl" mb="1rem" bg={profileCardBg}>
+      <Flex
+        flexDirection="row"
+        style={style.ProfileCard}
+        borderRadius="xl"
+        mb="1rem"
+        bg={profileCardBg}
+      >
         <Stack direction="row" spacing="0px">
-          <UserImage boxShadow="lg"/>
+          <UserImage boxShadow="lg" />
           <UserDetails />
         </Stack>
       </Flex>
